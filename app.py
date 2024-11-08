afb74cdec3b19cc29ca4f63be619bc1738966e69
davidluby <davidluby273@gmail.com>
2024-11-08 17:26:46 -0500

    fixed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # Date Created: 2/15/2023
 #
 """
-
+#
 # Package imports 
 from flask import Flask, jsonify, session, request, redirect, url_for, render_template
 #from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, get_jwt
